882138360f3fd5e84ad8044582af4b2f8c4a2635
MIKE <mike@MacBook-Air.local>
2023-12-17 15:41:14 +0300

    added 4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
     namespace App\Providers;
 
+    use Illuminate\Support\Facades\Artisan;
     use Illuminate\Support\ServiceProvider;
 
     class AppServiceProvider extends ServiceProvider
@@ -23,7 +24,7 @@
          */
         public function boot ()
         {
-//            Artisan ::call( 'migrate' );
+            Artisan ::call( 'migrate' );
 //            Artisan ::call( 'db:seed' );
         }
     }
